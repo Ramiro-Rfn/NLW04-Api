7390843df22d3218e4762293ca9b7e82a8ac9418
Ramiro_Nzau <ramirofrancisco962014@gmail.com>
2021-02-24 04:10:55 +0100

    configurando o banco de dados,typeorm e inicializando o typescript

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import 'reflect-metadata';
 
 const app = express();
+import './database';
 
 app.get('/', (request, response)=>{
     response.json({message: 'Hello NLW04'});
 });
 
-app.listen(3434,()=>{
+app.listen(3333,()=>{
     console.log("Server started in localhost:3434");
 });
