a0d33081f1893d2e0514f4b5185bbc509b77f895
Ramiro_Nzau <ramirofrancisco962014@gmail.com>
2021-02-27 17:53:02 +0100

    criando o nosso model de user e terminando o CRUD de criação de usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController{
     async create(request: Request, response: Response){
-        const body = request.body;
+        const { name , email} = request.body;
 
-        console.log(body);
+        const userRepository = getRepository(User);
 
-        response.status(201).json(body)
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        })
+        
+        if(userAlreadyExists){
+            return response.status(400).json({
+                error: 'User already exists!'
+            })
+        }
+
+        const user = userRepository.create({
+            name,
+            email
+        });
+
+        await userRepository.save(user);
+
+        return response.status(201).json(user);
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,7 @@ import { routes } from './routes';
 const app = express();
 
 app.use(express.json())
-
 app.use(routes);
-
-
 
 app.listen(3333,()=>{
     console.log("Server started in localhost:3333");
