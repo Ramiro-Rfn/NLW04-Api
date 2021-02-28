88a403697a63393d4b48b08f6351a5a9f2ce65af
Ramiro_Nzau <ramirofrancisco962014@gmail.com>
2021-02-28 01:30:56 +0100

    criado a migration de Surveys e adicionando o uuid

---
+++ src/database/migrations/1614445555719-CreateSurveys.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveys1614445555719 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'Surveys',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true
+                    },
+
+                    {
+                        name: 'title',
+                        type: 'varchar'
+                    },
+                    
+                    {
+                        name: 'description',
+                        type: 'varchar'
+                    },
+
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('Surveys')
+    }
+
+}
